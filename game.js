1b0f8c53308a4b18f2bb2fe1513260afa734c713
Krzysztof Kampa <krzysztofkampa7@gmail.com>
2020-06-02 16:50:25 +0200

    add player choose-option function in game.js, html fix

---
+++ game.js
@@ -0,0 +1,26 @@
+const gameSummary = {
+    numbers: 0,
+    wins: 0,
+    losses: 0,
+    draws: 0
+}
+
+const game = {
+    playerHand: "",
+    aiHand: "",
+    playerHandHTML: ""
+}
+
+const hands = [...document.querySelectorAll(".select img")];
+
+function handSelection() {
+    game.playerHand = this.dataset.option
+    hands.forEach(hand => hand.style.boxShadow = "");
+    this.style.boxShadow = "0 0 0 4px yellow";
+}
+
+hands.forEach(hand => hand.addEventListener("click", handSelection));
+
+
+
+
